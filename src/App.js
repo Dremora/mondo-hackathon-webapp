c4cb93aaadb59ad7d34110cff1d1f520e60a1c6b
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-11-14 16:20:21 +0000

    Pass data from top level

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import gaussian from 'gaussian'
+import _ from 'lodash'
 
 import Dashboard from './Dashboard';
 import ChartWrapper from './ChartWrapper';
 
 export class App extends Component {
   render() {
+    const days = 30
+    const maxToSpend = 500
+    const distribution = gaussian(maxToSpend / days, 50)
+    const data = _.range(1, days + 1).map(day => {
+      return Math.ceil(distribution.ppf(Math.random()))
+    })
+
     return (
       <div>
         <Dashboard daily={100} balance={200}/>
         <div className="report">
           <h2>tracking your progress...</h2>
-          <ChartWrapper onMonthChange={() => {}}/>
+          <ChartWrapper onMonthChange={() => {}} maxToSpend={maxToSpend} data={data}/>
       	</div>
       </div>
     );
--- src/Chart.js
+++ src/Chart.js
@@ -2,35 +2,27 @@ import React, { Component } from 'react'
 import { VictoryChart, VictoryLine, VictoryAxis } from "victory"
 import _ from 'lodash'
 import Immutable from 'immutable'
-import gaussian from 'gaussian'
 import moment from 'moment'
-
-const maxToSpend = 500
-const days = 30
-const distribution = gaussian(maxToSpend / days, 50)
-
-const ideal = [
-  {x: 1, y: 0},
-  {x: 30, y: maxToSpend}
-]
-
-const spentPerDay = _.range(1, days + 1).map(day => {
-  return Math.ceil(distribution.ppf(Math.random()))
-})
-
-const transactionData = _.foldl(spentPerDay, (acc, day) => {
-  if (acc.size === 0) {
-    return Immutable.List.of(day)
-  } else {
-    return acc.push(acc.get(acc.size - 1) + day)
-  }
-}, Immutable.List.of())
-.toArray()
-.map((val, index) => ({ x: index + 1, y: val }))
-
 
 export default class Chart extends Component {
   render() {
+    const { maxToSpend, data } = this.props
+    const days = data.length
+    const ideal = [
+      {x: 1, y: 0},
+      {x: days, y: maxToSpend}
+    ]
+
+    const transactionData = _.foldl(data, (acc, day) => {
+      if (acc.size === 0) {
+        return Immutable.List.of(day)
+      } else {
+        return acc.push(acc.get(acc.size - 1) + day)
+      }
+    }, Immutable.List.of())
+    .toArray()
+    .map((val, index) => ({ x: index + 1, y: val }))
+
     return (
       <VictoryChart>
         <VictoryAxis
